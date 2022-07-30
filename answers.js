64fab0bde332699bc7760b40f9a5b36da73542f5
Joseph Robinson <josephr1981@live.com>
2022-07-29 20:22:54 -0400

    Fizz Buzz answered

--- answers.js
+++ answers.js
@@ -12,11 +12,23 @@ for (let i = 0; i <= 200; i += 2) {
        console.log(i); 
 }
 
-
 ////////////////////////////////
 // Fizz Buzz
 ////////////////////////////////
-
+for (i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz');
+    }
+    else if (i % 5 === 0) {
+        console.log('Buzz');
+    }
+    else if (i % 3 === 0) {
+        console.log('Fizz');
+    }
+    else {
+        console.log(i);
+    }
+}
 
 ////////////////////////////////
 // Wild Wild Life

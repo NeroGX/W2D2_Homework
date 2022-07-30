d4996559e6f7276861ccb18d25aaaf28c918a373
Joseph Robinson <josephr1981@live.com>
2022-07-30 14:05:51 -0400

    Wild Wild Life & Yell at Ninja Turtles code added.

--- answers.js
+++ answers.js
@@ -33,13 +33,33 @@ for (i = 1; i <= 100; i++) {
 ////////////////////////////////
 // Wild Wild Life
 ////////////////////////////////
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+console.log(plantee);
+plantee[2]++;
+console.log(plantee);
+console.log(wolfy);
+wolfy[3] = "Gotham City";
+console.log(wolfy);
+console.log(dart);
+dart.push('Hawkins');
+console.log(dart);
+wolfy.shift();
+wolfy.unshift('Gameboy');
+console.log(wolfy);
 
 
 ////////////////////////////////
 // Yell at the Ninja Turtles
 ////////////////////////////////
-
-
+const ninjaTurtles = ['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo']  // Dev note: Raphael is the best!! No counter-arguments
+for (const element of ninjaTurtles) {
+    console.log(element.toUpperCase());
+}
 ////////////////////////////////
 // Methods, Revisited
 ////////////////////////////////
